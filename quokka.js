b5d0cb0b28d2e9f4983c62416b44edf6fa5274bb
Artem Govorov <artem.govorov@gmail.com>
2017-03-21 12:57:51 +1000

    Initial commit

---
+++ quokka.js
@@ -0,0 +1,16 @@
+// The config also be placed into package.json or global quokka config,
+// see https://quokkajs.com/docs/configuration.html
+({
+    babel: true,
+    plugins: ['jsdom-quokka-plugin']
+})
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.body.innerHTML += `<div id="root"></div>`;
+
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
