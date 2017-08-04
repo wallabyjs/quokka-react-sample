1493288fd74ee6484ce5095f05906685fd771cd6
Artem Govorov <artem.govorov@gmail.com>
2017-08-04 10:16:11 +1000

    Initial commit

---
+++ src/modules/welcome/test.js
@@ -0,0 +1,14 @@
+({
+  babel: true
+});
+
+import React, {Component} from 'react';
+
+const connect = () => () => {};
+const mapStateToProps = () => {};
+const mapDispatchToProps = () => {};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class Welcome extends Component {
+  render() {}
+}
