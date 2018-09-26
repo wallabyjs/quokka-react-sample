acb98f542d35f3dd3bcff8a3112c768723624352
Simon McEnlly <simon.mcenlly@gmail.com>
2018-09-26 13:24:41 +1000

    Updated quokka-react-sample to use Babel 7

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = api => {
+  api.cache.forever();
+  return {
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react"
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread"
+    ]
+  };
+}
